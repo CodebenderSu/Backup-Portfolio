8c8cb2b25154d2b5cbde6f387cdbcdd4c88d7821
Su <codebendersu@gmail.com>
2018-06-06 19:38:26 -0700

    [Feature] WIP Animation
    And some other minor changes

---
+++ src/components/Airbending.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Airbending extends Component {
+  render() {
+    return (
+      <div className="airbending">
+        <i class="fab fa-envira" />
+      </div>
+    );
+  };
+};
+
+export default Airbending;
--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Airbending from './Airbending.js';
 import * as avatar from '../utils/images/avatar.png';
 import * as air from '../utils/images/air.png';
 import * as greatestChange from '../utils/audio/greatest-change.mp3';
@@ -8,14 +9,16 @@ class Avatar extends Component {
     let flip = this.props.status ? 'avatar-flip' : '';
     return (
       <div className="avatar-container">
+        <Airbending />
         <div className={"avatar " + flip} onClick={this.props.handleAvatarClick}>
-          <div className="avatar-front">
+          <div className="avatar-front"> {/* Front Side */}
             <img src={avatar} alt="Avatar"
                  title="Fancy Schmancy Avatar" />
           </div>
-          <div className="avatar-back">
+          <div className="avatar-back"> {/* Back Side */}
             <img src={air} alt="Air" />
-            <audio className="audio-player" controls>
+            <audio className="audio-player" controls
+                   title="'Greatest Change' by Jeremy Zuckerman">
               <source src={greatestChange} type="audio/mp3" />
               Your browser does not support this feature.
             </audio>
