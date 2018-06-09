e10a440865edba543c1e539230d9b105d6278239
Su <codebendersu@gmail.com>
2018-06-08 18:11:41 -0700

    [Feature] Contact buttons
    TODO: finish styling. Flexbox is being ornery with the <a> and <p> tags

--- src/components/DrawerContact.js
+++ src/components/DrawerContact.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import * as Flavor from '../flavor.json';
 import * as fire from '../utils/images/fire.png';
+import * as gmail from '../utils/images/gmail.png';
+import * as github from '../utils/images/github.png';
+import * as linkedin from '../utils/images/linkedin.png';
 
 class DrawerContact extends Component {
   render() {
@@ -10,7 +13,25 @@ class DrawerContact extends Component {
         <div className="right-drawer-content">
           <h1 className="drawer-header">{Flavor.Contact.Header}</h1>
           <p>{Flavor.Contact.Paragraph1}</p>
-          <p>{Flavor.Contact.Paragraph2}</p>
+          <div className="contact">
+            <span>
+            <a href="mailto:codebendersu@gmail.com">
+              <img src={gmail} alt="Gmail" title="Email me" />
+            </a>
+            </span>
+            <span>
+            <a href="https://github.com/CodebenderSu">
+              <img src={github} alt="Github" title="My Github profile" />
+            </a>
+            </span>
+            <span>
+            <a href="https://www.linkedin.com/in/susan-l-3b388a13a/">
+              <img src={linkedin} alt="LinkedIn" title="My LinkedIn" />
+            </a>
+            </span>
+            <p className="subtitle">{Flavor.Contact.Paragraph2}</p>
+          </div>
+
         </div>
         <img className="drawer-footer" src={fire} alt="fire" />
         <div className="right-drawer-tab" onClick={this.props.handleContactClick}>
